d7fc9b1fff69395f7f9276808379ba8a4f4f7ce1
Oleksandr Dubel <alex.dubel@gmail.com>
2020-01-15 11:31:42 +0200

    get-name-from-ip-4-home.ps1 modified. Final version I hope.

--- get-name-from-ip-4-home.ps1
+++ get-name-from-ip-4-home.ps1
@@ -1,13 +1,16 @@
 #$Outfile="C:\temp\ipaddr\ipaddr-2-rez.txt"
 #$Outfile="M:\Scripts\Powershell\get-name-from-ip\ipaddr-2-rez.txt"
-$Outfile="C:\Users\adm-odubel\Documents\PowerShell\Scripts\get-name-from-ip\ipaddr-2-rez.csv"
+$PathToFile="C:\Users\adm-odubel\Documents\PowerShell\Scripts\get-name-from-ip"
+$Outfile="$PathToFile\ipaddr-2-rez.csv"
 Clear-Content $Outfile
 #$Error.Clear()
 #$name = "77.222.159.230", "185.34.224.179", "149.154.161.20" ,"149.154.161.3", "google.com"
-$name=Get-Content "C:\Users\adm-odubel\Documents\PowerShell\Scripts\get-name-from-ip\ipaddr-1.txt"
+$name=Get-Content "$PathToFile\ipaddr-1.txt"
 #try {
-    $name | ForEach-Object { $dns = Resolve-DnsName $PSItem  -DnsOnly -ErrorAction SilentlyContinue   
-    $infoObject = New-Object PSObject
+    #Do not use SilentlyContinue because it will suppress all errors
+    $name | ForEach-Object { $dns = Resolve-DnsName $PSItem  -DnsOnly -Server 8.8.4.4 -ErrorAction Continue   
+        Write-Debug "$PSItem" #-ForegroundColor Yellow
+        $infoObject = New-Object PSObject
     Add-Member -inputObject $infoObject -memberType NoteProperty -name "Address"				    -value $PSItem
     #Add-Member -inputObject $infoObject -memberType NoteProperty -name "DNSType"				    -value $dns.Type
     if ($dns.Namehost -eq $null) {
@@ -17,18 +20,6 @@ $name=Get-Content "C:\Users\adm-odubel\Documents\PowerShell\Scripts\get-name-fro
              { Add-Member -inputObject $infoObject -memberType NoteProperty -name "DNSNamehost"     -value "DNS name does not exist"  }
         else { Add-Member -inputObject $infoObject -memberType NoteProperty -name "DNSNamehost"     -value  $Error[0].Exception.Message }
     }
-    else {
-    Add-Member -inputObject $infoObject -memberType NoteProperty -name "DNSNamehost"                -value $dns.Namehost}
+    else {     Add-Member -inputObject $infoObject -memberType NoteProperty -name "DNSNamehost"     -value $dns.Namehost }
     $infoObject | Export-Csv -path $OutFile -NoTypeInformation -Encoding UTF8 -Append #Export the results in csv file.
     } 
-#} 
-<#
-#Catch{
-    $infoObject = New-Object PSObject
-    Add-Member -inputObject $infoObject -memberType NoteProperty -name "Address"				    -value $PSItem
-    #Add-Member -inputObject $infoObject -memberType NoteProperty -name "DNSType"				    -value $dns.Type
-    #Add-Member -inputObject $infoObject -memberType NoteProperty -name "DNSNamehost"                -value $dns.Namehost
-    Add-Member -inputObject $infoObject -memberType NoteProperty -name "DNS"                        -value $Error[0].Exception.Message
-    $infoObject | Export-Csv -path $OutFile -NoTypeInformation -Encoding UTF8 -Append #Export the results in csv file.
-#} 
-#>
